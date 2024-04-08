85b24b47ba6f34431e1d0172dba5da27d4f1f683
AdoShan <44154107+AdoShan@users.noreply.github.com>
2024-04-08 15:37:20 +0800

    add Xmind Crack

--- Script/Xmind.js
+++ Script/Xmind.js
@@ -1,16 +1,40 @@
-/*
-脚本功能：解锁Xmind思维导图订阅到2030年
-脚本作者：R·E
-脚本发布：https://t.me/Functional_Store_Hub
+/*************************************
 
-更新时间：2022.08.13
-支持版本：商店版本 
-下载地址：https://apps.apple.com/app/id1286983622
+项目名称：Xmind-思维导图
+下载地址：https://t.cn/AipCL5zE
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+使用说明：先登录Xmind账号在恢复购买
 
-使用声明：⚠️⚠️⚠️此脚本仅供学习与交流,禁止转载与贩卖,需在24小时内从设备中删除！⚠️⚠️⚠️
-*/
+**************************************
 
-var _0x49754b = JSON['parse']($response['body']);
-_0x49754b['license']['status'] = 'sub';
-_0x49754b['license']['expireTime'] = 0x1b8d90e4000;
-$done({'body': JSON['stringify'](_0x49754b)});
+[rewrite_local]
+^https?:\/\/(?:www\.)?xmind\..*\/.+\/(devices|token\/.+) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Xmind.js
+
+[mitm]
+hostname = *xmind.*
+
+*************************************/
+
+
+var chxm1023 = JSON.parse($response.body);
+const vip = /https:\/\/www\.xmind\.cn\/_res\/devices/;
+const token = /https:\/\/xmind\.cn\/_res\/token\/.+/;
+
+if (vip.test($request.url)) {
+    chxm1023 = {
+      "license" : {
+        "status" : "sub",
+        "expireTime" : 4092599349000
+      },
+      "_code" : 200,
+      "raw_data" : "GfxYgAqnrQ\/ggD9UwqnZyAj6FKnopXzM8s5m3eZLOsmVr4FwCzv41qTmgi\/u72cv+jpaAoljaEPti1twzOS7X7KUPY1KNJ2xalbS7SXbvFHSvs21QXjaUtIOkeJWAl4\/vHl4IvMeHTBVqD8mFCXOmvnBPLRMAJEPgHEJYF1InvQ="};
+}
+
+if (token.test($request.url)) {
+    chxm1023.expireDate = 4092599349;
+    chxm1023.token = "f50633ea8eb04cbb85962b99c47045d7AjOobEGo";
+}
+
+$done({body : JSON.stringify(chxm1023)});
